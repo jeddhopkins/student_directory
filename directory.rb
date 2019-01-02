cd9133d7d699db9d8d71f1abc4badeb59ef667f0
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-01-02 23:57:40 +0000

    Added method to load students from command line

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -42,14 +42,17 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} great students!"
+    if @students.count == 1 then puts "Now we have 1 great student"
+    else
+      puts "Now we have #{@students.count} great students!"
+    end
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -65,7 +68,10 @@ def print_students_list
 end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students!"
+  if @students.count == 1 then puts "Overall, we have 1 great student"
+  else
+    puts "Overall, we have #{@students.count} great students!"
+  end
 end
 
 def save_students
@@ -80,8 +86,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -89,5 +95,17 @@ def load_students
   file.close
 end
 
-# nothing happens until we call the method
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isnt given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist"
+    exit # quit the program
+  end
+end
+
+try_load_students
 interactive_menu
